3ef6320ef681c94adfd8f6f4132b48066faa71d8
badichiboy <hod.badichi@campus.technion.ac.il>
2021-05-07 19:51:04 +0000

    changes

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -1,13 +1,41 @@
-.globl my_ili_handler
-.extern what_to_do
+.globl ili_handler
 
 .text
 .align 4, 0x90
-my_ili_handler:
-	#prologue
-	add $0x1,0(%rsp)
-	call what_to_do
-	mov $0x5,%rax
-	iretq
+ili_handler:
+#prologue
+   #just for testing
+  pushq %r8
+  push %rax
+#checking what opcode length
+  mov $0x27,%rbx
+  mov %rbx,16(%rsp)
+  testq $0xf00,16(%rsp)
+  jnz len2
+len1:
+  movq $1, %r8
+  jmp function
+len2:
+  movq $2, %r8
+function:
+#calling what_to_do with last byte of opcode
+  movq $0xff, %rdi
+  andq 16(%rsp),  %rdi
+  call what_to_do
+#cases in accordence to function return value
+  cmp $0, %eax
+  jne case_not_zero 
+case_zero:
+   popq %rax
+   popq %r8
+   popq %rdi
+   jmp old_ili_handler
+case_not_zero:
+   mov %rax,%rdi
+   popq %rax
+   add %r8,16(%rsp)
+   popq %r8
+   iretq
+  
 
     
